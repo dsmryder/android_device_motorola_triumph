92bb22164235e4f792330b1fb85661c8572d42af
dsmryder <dsmryder@yahoo.com>
2012-07-16 23:07:15 -0300

    Moving to small language pack

--- triumph.mk
+++ triumph.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_small.mk)
 
 # The gps config appropriate for this device
 #$(call inherit-product, device/common/gps/gps_us_supl.mk)
